11ca04b570983ced4b3cc24ea0d6576d216ae1fb
Ruslan Tokar <djrexxx87@gmail.com>
2018-01-28 20:47:43 +0200

    add paginations

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,10 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from django.http import HttpResponse
 from news.models import News
 
 
 def main_page(request):
-	news = News.objects.all().order_by('-pk')
+	news_list = News.objects.all().order_by('-pk')
+	paginator = Paginator(news_list, 2) # Show 4 news per page
+
+	page = request.GET.get('page')
+	try:
+		news = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		news = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		news = paginator.page(paginator.num_pages)
+
 	return render(request, 'mainpage.html', {'news': news})
--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import F
@@ -7,7 +8,19 @@ from models import News
 
 
 def news_list(request):
-	news = News.objects.all().order_by('-pk')
+	news_list = News.objects.all().order_by('-pk')
+	paginator = Paginator(news_list, 4) # Show 4 news per page
+
+	page = request.GET.get('page')
+	try:
+		news = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		news = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		news = paginator.page(paginator.num_pages)
+
 	return render(request, 'news.html', {'news': news})
 
 def news_detail(request, pk):
